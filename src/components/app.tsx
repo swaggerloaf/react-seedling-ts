cebf577e124ebd8db7ca575bbb320edfb44b423c
swaggerloaf <swaggerloaf@gmail.com>
2017-09-01 22:04:13 -0400

    Updates

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
 
-export interface AppProps { compiler: string; framework: string; }
+export interface AppProps { name: string;}
 
 // 'HelloProps' describes the shape of props.
 // State is never set so we use the 'undefined' type.
 export class App extends React.Component<AppProps, undefined> {
     render() {
-        return <h1>Hello braski from {this.props.compiler} and {this.props.framework}!</h1>;
+        return <h1>Let's go for a {this.props.name } spin</h1>;
     }
 }
