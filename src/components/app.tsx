32f4dff2a2e18bf0c37c04f47c8a426b99b15eff
swaggerloaf <swaggerloaf@gmail.com>
2017-09-06 21:31:30 -0400

    Updates

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -6,6 +6,6 @@ export interface AppProps { name: string;}
 // State is never set so we use the 'undefined' type.
 export class App extends React.Component<AppProps, undefined> {
     render() {
-        return <h1>Let's go for a {this.props.name } spin</h1>
+        return <h1>Let's go for a {this.props.name } react spin!!</h1>
     }
 }
